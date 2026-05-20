1104a0b593fe46d0545c435f8fe14d868af6694e
semantic-release <semantic-release>
2026-05-20 15:24:06 +0000

    chore(release): v1.22.0 [skip ci]

--- src/dracs/__init__.py
+++ src/dracs/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-__version__ = "1.21.1"
+__version__ = "1.22.0"
 
 from dracs.exceptions import (  # noqa: F401
     APIError,
--- src/dracs_client/__init__.py
+++ src/dracs_client/__init__.py
@@ -1,3 +1,3 @@
 """DRACS Client - remote CLI for querying a DRACS server."""
 
-__version__ = "1.21.1"
+__version__ = "1.22.0"
